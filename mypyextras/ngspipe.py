2399ece7759f59616833d529282f313e6610f5b1
Panciera <michael.panciera@amedwrair024635.amed.ds.army.mil>
2016-03-10 17:09:18 -0500

    reorg and added bamsort

--- mypyextras/ngspipe.py
+++ mypyextras/ngspipe.py
@@ -1,4 +1,5 @@
 from ioutils import File, sh, StatsFile, FastaIndex, Fastq, Fasta, Bam, VCF, GZip, PairedEnd, Platform
+from pipeliine import build_pipeline, order_funcs
 from typing import Callable, Tuple, List, TypeVar, Iterator, Set, Any, NamedTuple, Iterable, Optional, Union
 from collections import Counter
 from itertools import starmap
@@ -6,8 +7,8 @@ A = TypeVar("A")
 B = TypeVar("B")
 C = TypeVar("C")
 
-TrimOpts = NamedTuple('TrimOpts', 
-        [('paired',bool), 
+TrimOpts = NamedTuple('TrimOpts',
+        [('paired',bool),
          ('trim_n', bool),
          ('q', Optional[int]),
          ('removebases', Optional[int]),
@@ -28,7 +29,7 @@ def gunzip(gz: GZip) -> Fastq:
 
 def samtools_flagstats(bam: Bam) -> StatsFile:
     stats = sh.samtools("flagstats", bam, _iter=True)
-    statsFile = StatsFile("flagstats.txt") 
+    statsFile = StatsFile("flagstats.txt")
     fields = ['in total', 'duplicates', ' mapped (', 'paired in sequencing', 'read1', 'read2', 'properly paired', 'with itself and mate mapped', 'singletons']
     with statsFile.open('w') as out:
         fieldsInLine = lambda s: filter(s.__contains__, fields) # type: Callable[[str], Iterable[str]]
@@ -47,16 +48,35 @@ def filter_fastq(fqs: PairedEnd, opts: FilterOpts) -> PairedEnd:
 
 def align_paired(fqs: PairedEnd, ref: FastaIndex) -> Bam:
     sh.bwa("mem", fqs[0], fqs[1])
-    return Bam("paired.bam") 
+    return Bam("paired.bam")
 
-def tagbam(bam: Bam, opts: TagBamOptions) -> Bam:
-    sh.tagreads(bam, CN=opts.CN, SM=opts.SM)
+def tagbam(bam: Bam, opts: TagBamOptions) -> Sam:
+    outSam = Bam(bam.drop("bam").tagged.bam)
+    sh.tagreads(bam, CN=opts.CN, SM=opts.SM, o=outSam)
+    return outSam
+
+just_bam = run('sam_to_bam', input=options['input'])
+sort_bam = run('sort_bam', input=just_bam, output=output)
+index_bam = run('index_bam', input=sort_bam)
+
+def toBamIndexSort(sam: Sam) -> (Bam, Index):
+    # sh can't do pipes properly
+    from plumbum.cmd import samtools
+    bam = Bam(sam.drop("sam").bam)
+    chain = samtools['view', sam, '-hb'] | \
+        samtools['sort', '-', bam.drop("bam")]
+    # samtools view tagged.sam -hb | samtools sort - tagged.bam
+    print chain
+    chain()
+    next = samtools['index', bam]
+    print next
+    next()
     return bam
 
 def freebayes(bam: Bam, ref: Fasta) -> VCF:
     vcf = VCF("freebayes.vcf")
-    sh.freebayes(bam, f=ref, _out=vcf.name)
-    return vcf 
+    sh.freebayes(bam, f=ref, _out=vcf)
+    return vcf
 
 def index_fasta(f: Fasta) -> List[FastaIndex]:
     index_extensions = ['amb', 'ann', 'bwt', 'pac', 'sa']
@@ -65,4 +85,15 @@ def index_fasta(f: Fasta) -> List[FastaIndex]:
     return outputs
 
 def consensus(bam: Bam, ref: Fasta, vcf: VCF) -> Fasta:
-    pass # run consensus
+    out = Fasta("conesnsus.fasta")
+    sh.consensus(**locals())
+    return out
+
+
+funcs = [consensus, freebayes, tagbam, align_paired, filter_fastq, trim_fastq, gunzip]
+res = build_pipeline(funcs, (id, {'return' : Tuple[Fastq, Fastq]}, []))
+print(res)
+nodes = (order_funcs(funcs, (id, {'return' : Tuple[Fastq, Fastq]}, [])))
+print(nodes)
+
+
--- mypyextras/pipeline.py
+++ mypyextras/pipeline.py
@@ -1,79 +1,30 @@
-from ioutils import *
-from typing import * 
+from typing import Tuple,Callable,Any,Dict,Union,List
+from ioutils import File
 from toolz import compose
 from gadt import partition
 from operator import itemgetter as get
 from toolz.dicttoolz import keyfilter
 from functools import partial, reduce
 
-class StatsFile(File): pass
-class FastaIndex(File): pass
-
-TagBamOptions = NamedTuple("TagBamOptions",
-                          [('CN', Optional[str]),
-                           ('SM', Optional[str])])
-
-FilterOpts = NamedTuple("FilterOpts",
-                        [('indexQualMin', int),
-                         ('drop_ns', bool),
-                         ('platforms', List[Platform])])
-
-def gunzip(gz: GZip) -> Fastq:
-    sh.gunzip(gz.name)
-    return Fastq(gz.drop("gz"))
-
-def samtools_flagstats(bam: Bam) -> StatsFile:
-    sh.samtools.flagstats(bam, _iter=True)
-    statsFile = StatsFile("flagstats.txt") 
-    fields = ['in total', 'duplicates', ' mapped (', 'paired in sequencing', 'read1', 'read2', 'properly paired', 'with itself and mate mapped', 'singletons']
-    with statsFile.open('w') as out:
-        fieldsInLine = lambda s: filter(s.__contains__, fields) # type: Callable[[str], Iterable[str]]
-        counts = Counter(map(fieldsInLine, stats))
-        lines = starmap("{}{}".format, counts.items())
-        out.writelines(lines)
-    return statsFile
-
-def trim_fastq(fqs: PairedEnd, opts: TrimOpts) -> PairedEnd:
-    cmd = sh.cutadapt.bake(removebases=opts.removebases, qualcutoff=opts.qualcutoff, trim_n=opts.trim_n)
-    cmd(fwd, rev)
-    return Fastq(fwd.trim), Fastq(rev.trim)
-
-def filter_fastq(fqs: PairedEnd, opts: FilterOpts) -> PairedEnd:
-    pass # run filter
-
-def align_paired(fqs: PairedEnd, ref: FastaIndex) -> Bam:
-    sh.bwa.mem(*fqs)
-    return Bam("paired.bam")
-
-
-def tagbam(bam: Bam, opts: TagBamOptions) -> Bam:
-    sh.tagreads(bam, CN=opts.CN, SM=opts.SM); return bam
-
-def freebayes(bam: Bam, ref: Fasta) -> VCF:
-    vcf = VCF("freebayes.vcf")
-    sh.freebayes(bam, f=ref, _out=vcf.name)
-    return vcf
-
-
-def index_fasta(f: Fasta) -> List[IndexFasta]:
-    index_extensions = ['amb', 'ann', 'bwt', 'pac', 'sa']
-    outputs = [IndexFasta(getattr(f, ext)) for ex in index_extensions]
-    sh.bowtie.index(f); return outputs
-
-def consensus(bam: Bam, ref: Fasta, vcf: VCF) -> Fasta:
-    pass # run consensus
-Func = Callable[...,Any]
-Node = Tuple[Func, Dict[str,type], Dict[str,type]]
+Node = Tuple[Callable[...,Any], Dict[str,type], Dict[str,type]]
 
 def get_pos_opt_args(func: Callable[...,Any]) -> Node:
     annotations = func.__annotations__
     is_opt = lambda x: x[0] == 'opts' # type: Callable[[Tuple[str,type]], bool]
-    pos_args, opt_args = partition(is_opt, annotations.items()) 
+    pos_args, opt_args = partition(is_opt, annotations.items())
     return func, dict(pos_args), dict(opt_args)
 
-def order_funcs(funcs: List[Func], input: Union[File, Tuple[File,File]]) -> List[Node]:
+def node_is_satisfied(acc: List[Node], node: Node) -> bool:
+    f, args, _ = node
+    required = keyfilter(lambda x: x != 'return', args) #rettype = args['return']
+    get_ret = lambda x: x[1]['return'] # type: Callable[[Node],type]
+    acc_rets = map(get_ret, acc)
+    acc_rets = list(acc_rets)
+    satisfied = all([(t in acc_rets) for t in required.values()])
+    return satisfied
+def order_funcs(funcs: List[Callable[...,Any]], input: Union[File, Tuple[File,File]]) -> List[Node]:
     nodes = map(get_pos_opt_args, funcs)
-    def fill_opts(node: Node) -> Func:
+    def fill_opts(node: Node) -> Callable[...,Any]:
         f, _, optargs = node
         if not optargs: return node
         assert len(optargs) == 1
@@ -82,31 +33,19 @@ def order_funcs(funcs: List[Func], input: Union[File, Tuple[File,File]]) -> List
     filled_nodes = list(map(fill_opts, nodes))
     def top_sort(acc: List[Node], to_go: List[Node]) -> List[Node]:
         if to_go == []: return acc
-        def is_satisfied(node: Node) -> bool:
-            f, args, _ = node
-            required = keyfilter(lambda x: x != 'return', args) #rettype = args['return']
-            get_ret = lambda x: x[1]['return'] # type: Callable[[Node],type]
-            acc_rets = map(get_ret, acc)
-            acc_rets = list(acc_rets)
-            satisfied = all([(t in acc_rets) for t in required.values()])
-            return satisfied
+        is_satisfied = lambda n: node_is_satisfied(acc, n) # type: Callable[[Node],bool]
         nextnode = next(filter(is_satisfied, to_go))
         next_to_go = list(filter(is_satisfied, to_go))[1:]
         return top_sort([nextnode] + acc, next_to_go)
     sorted = top_sort([input], filled_nodes)
     return sorted
 
-def build_pipeline(funcs: List[Func], input) -> Func:
+def build_pipeline(funcs: List[Callable[...,Any]], input) -> Callable[...,Any]:
     nodes = order_funcs(funcs, input)
     ordered_funcs = map(get(0), nodes)
     return reduce(compose, ordered_funcs)
 
-funcs = [consensus, freebayes, tagbam, align_paired, filter_fastq, trim_fastq, gunzip]
-res = build_pipeline(funcs, (id, {'return' : Tuple[Fastq, Fastq]}, []))
-print(res)
-nodes = (order_funcs(funcs, (id, {'return' : Tuple[Fastq, Fastq]}, [])))
-print(nodes)
 # file_funcs = dir(__file__)
 
-# most, if not all the difficulty of writing this could've been mitigated by pinning down the types . . . 
+# most, if not all the difficulty of writing this could've been mitigated by pinning down the types . . .
 # worth biting the bullet, especially for higher-order functions right now
